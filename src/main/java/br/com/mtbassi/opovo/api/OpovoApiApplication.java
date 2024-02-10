185683e94d02b655ea2301d8096b20a188c0fb22
Matheus Bassi <bassi.matheus@outlook.com>
2024-02-09 21:27:05 -0300

    first commit

---
+++ src/main/java/br/com/mtbassi/opovo/api/OpovoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.mtbassi.opovo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpovoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpovoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mtbassi/opovo/api/OpovoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mtbassi.opovo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpovoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
