62448f30153e2b86bee8b1f76f749b1f6079d6cc
Matheus Bassi <bassi.matheus@outlook.com>
2024-02-18 18:12:28 -0300

    feat: mapping exception

--- src/main/java/br/com/mtbassi/opovo/api/modules/auth/services/AuthenticationService.java
+++ src/main/java/br/com/mtbassi/opovo/api/modules/auth/services/AuthenticationService.java
@@ -3,6 +3,7 @@ package br.com.mtbassi.opovo.api.modules.auth.services;
 import br.com.mtbassi.opovo.api.infra.security.TokenService;
 import br.com.mtbassi.opovo.api.modules.auth.dto.LoginRequest;
 import br.com.mtbassi.opovo.api.modules.auth.dto.LoginResponse;
+import br.com.mtbassi.opovo.api.modules.commons.exceptions.ModelException;
 import br.com.mtbassi.opovo.api.modules.journalists.entities.JournalistEntity;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -19,10 +20,14 @@ public class AuthenticationService {
     private final ModelMapper modelMapper;
 
     public LoginResponse login(LoginRequest data) {
-        var usernamePassword = new UsernamePasswordAuthenticationToken(data.getEmail(), data.getPassword());
-        var auth = this.authenticationManager.authenticate(usernamePassword);
-        return LoginResponse.builder()
-                .token(tokenService.generateToken(modelMapper.map(auth.getPrincipal(), JournalistEntity.class)))
-                .build();
+        try {
+            var usernamePassword = new UsernamePasswordAuthenticationToken(data.getEmail(), data.getPassword());
+            var auth = this.authenticationManager.authenticate(usernamePassword);
+            return LoginResponse.builder()
+                    .token(tokenService.generateToken(modelMapper.map(auth.getPrincipal(), JournalistEntity.class)))
+                    .build();
+        } catch (RuntimeException e) {
+            throw new ModelException("Login failed.", e);
+        }
     }
 }
