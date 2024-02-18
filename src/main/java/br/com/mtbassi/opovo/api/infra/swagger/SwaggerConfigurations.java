573ca827bdb033245146c999e007d661c5240b5d
Matheus Bassi <bassi.matheus@outlook.com>
2024-02-18 18:24:06 -0300

    feat: implement swagger in the authentication controller

--- src/main/java/br/com/mtbassi/opovo/api/infra/swagger/SwaggerConfigurations.java
+++ src/main/java/br/com/mtbassi/opovo/api/infra/swagger/SwaggerConfigurations.java
@@ -1,9 +1,12 @@
 package br.com.mtbassi.opovo.api.infra.swagger;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,14 +15,29 @@ public class SwaggerConfigurations {
 
     @Bean
     public OpenAPI openAPI() {
-        return new OpenAPI().info(info());
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes("Bearer Authentication", createApiKeyScheme()))
+                .info(info());
     }
 
     private Info info() {
-        return new Info().title("API O Povo").description("Service for newspaper journalists").version("v0.0.1").license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0")).contact(contact());
+        return new Info().title("O Povo Api")
+                .description("Service for newspaper journalists")
+                .version("v0.0.1")
+                .license(new License().name("Apache 2.0")
+                        .url("https://www.apache.org/licenses/LICENSE-2.0"))
+                .contact(contact());
     }
 
     private Contact contact() {
-        return new Contact().name("Matheus Bassi").email("bassi.matheus@outlook.com").url("https://www.linkedin.com/in/matheusbassi/");
+        return new Contact().name("Matheus Bassi")
+                .email("bassi.matheus@outlook.com")
+                .url("https://www.linkedin.com/in/matheusbassi/");
+    }
+
+    private SecurityScheme createApiKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
     }
 }
--- src/main/java/br/com/mtbassi/opovo/api/modules/auth/controllers/AuthenticationController.java
+++ src/main/java/br/com/mtbassi/opovo/api/modules/auth/controllers/AuthenticationController.java
@@ -1,8 +1,14 @@
 package br.com.mtbassi.opovo.api.modules.auth.controllers;
 
+import br.com.mtbassi.opovo.api.infra.exception_handler.dto.RestErrorMessage;
 import br.com.mtbassi.opovo.api.modules.auth.dto.LoginRequest;
 import br.com.mtbassi.opovo.api.modules.auth.dto.LoginResponse;
 import br.com.mtbassi.opovo.api.modules.auth.services.AuthenticationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -14,10 +20,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/login")
 @RequiredArgsConstructor
+@Tag(name = "Login", description = "Contains the operation for a user login.")
 public class AuthenticationController {
 
     private final AuthenticationService service;
 
+    @Operation(summary = "Authenticates the user.",
+            description = "Resource returns a jwt token when logging in with email and password.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Token generated successfully.",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = LoginResponse.class))),
+                    @ApiResponse(responseCode = "500", description = "Login failed.",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = RestErrorMessage.class)))
+            })
     @PostMapping
     public ResponseEntity<LoginResponse> login(@RequestBody @Valid LoginRequest data) {
         return ResponseEntity.ok(this.service.login(data));
